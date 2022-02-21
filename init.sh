77fc9306d5e216729bb3236d6bd76d12fd285a70
Harpo Jaeger <harpo.jaeger@gmail.com>
2022-02-21 13:13:40 -0500

    add comments

--- init.sh
+++ init.sh
@@ -1,3 +1,19 @@
 #! /bin/sh
 
-find /home/harpo/dotfiles -not \( -path /home/harpo/dotfiles/.git -prune \) -name .\* -print0 | xargs -0 -I {} bash -c 'ln -s $0 ~/${0##*/} && echo "created symlink to $0"' "{}"
+find /home/harpo/dotfiles \
+`# Exclude files in the .git directory` \
+-not \( -path /home/harpo/dotfiles/.git -prune \) \
+`# Find files beginning with a dot.` \
+-name .\* \
+`# Pipe the files into a bash script` \
+-print0 | xargs -0 -I {} bash -c '
+# Create a symlink for each file in the home directory.
+# The snazzy parameter substitution allows the target files to be as deeply
+# nested as is convenient, while symlinks are all place directly in ~/.
+ln -s $0 ~/${0##*/}
+# If the symlinking failed, an explicit error message is echoed. Otherwise, let
+# the user (me) know that it succeeded.
+if [ $? -eq 0 ]; then
+	echo "created symlink to $0"
+fi
+' "{}"
